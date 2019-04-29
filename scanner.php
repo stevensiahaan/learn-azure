27e575faf9fc1d76d363c205be052784632ca0d3
stevensiahaan <stevenjhsiahaan@gmail.com>
2019-04-29 14:37:45 +0700

    using real image

--- scanner.php
+++ scanner.php
@@ -46,8 +46,7 @@ $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
     $createContainerOptions->addMetaData("key2", "value2");
 
     $containerName = "blockblobs".generateRandomString();
-	// $fileToUpload = $_FILES['fileToUpload'];
-    $fileToUpload = "HelloWorld.txt";
+	$fileToUpload = $_FILES['fileToUpload'];
 
     if(isset($_POST["submit"])) {
 	try {
